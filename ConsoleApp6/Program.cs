1961eadc08e17ca8c6d4b0041479eb78e7a58ed8
Natalia <natabilcuk@gmail.com>
2024-09-23 14:47:56 +0300

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,16 @@
+﻿while (true)
+{
+    try
+    {
+        Console.WriteLine("Enter the 2 numbers you want to divide(a/b)");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = a / b;
+        int c1 = a % b;
+        Console.WriteLine("Dividing these numbers, it turned out: " + c + ", " + c1);
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("An error has occurred: You can't divide by zero!");
+    }
+}
